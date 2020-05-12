082e94903fb5b5bd3b9700f18a4c5b81e0b91e1e
NakshatraCodes <nakshatra163@gmail.com>
2020-05-12 20:36:50 +0530

    Made some changes to function

--- app.js
+++ app.js
@@ -2,42 +2,32 @@ const puppeteer = require('puppeteer');
 var d = new Date;
 var date = d.getDate() + '-' + (d.getMonth()+1) + '-' + d.getFullYear();
 
-const takescreenshot = async ()=>{
+const scrape = async (url, type)=>{
 
 	const browser = await puppeteer.launch();
-
 	const page = await browser.newPage();
-	const options = {
+
+	await page.goto(url);
+	await page.setViewport({ width: 1680, height: 1050 });
+
+	if(type=='jpeg'){
+		const options = {
 		path: `C:/Users/naksh/Pictures/Github-Screenshots/${date}.jpeg`,
 		fullPage: true,
 		type: 'jpeg',
 		quality: 100
+		}
+		await page.screenshot(options);
+	} else if(type=='pdf'){
+		await page.pdf({
+	    	path: `C:/Users/naksh/Pictures/Github-Screenshots/${date}.pdf`,
+	   		format: 'A4'
+	  	});
 	}
-
-	await page.goto('https://github.com/NakshatraCodes');
-	await page.setViewport({ width: 1680, height: 1050 });
-	await page.screenshot(options);
-
+	
 	await browser.close();
 	console.log('Done');
 }
 
-const takepdf = async ()=>{
-
-	const browser = await puppeteer.launch();
-
-	const page = await browser.newPage();
-
-	await page.goto('https://github.com/NakshatraCodes', {waitUntil: 'networkidle2'});
-	await page.setViewport({ width: 1680, height: 1050 });
-	  await page.pdf({
-	    path: `C:/Users/naksh/Pictures/Github-Screenshots/${date}.pdf`,
-	    format: 'A4'
-	  });
-
-	await browser.close();
-	console.log('Done');
-}
-
-takescreenshot();
-takepdf();
+// Specify the url and type as scrape(url, pdf)
+scrape('https://github.com/NakshatraCodes', 'pdf');
